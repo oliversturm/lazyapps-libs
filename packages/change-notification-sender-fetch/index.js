96f293516c4b0efceb9f004a71131ff64933324b
Oli Sturm <mail@oliversturm.ch>
2026-03-25 21:20:36 +0000

    feat: support jwt as function in change-notification-sender-fetch #publish-branch

--- packages/change-notification-sender-fetch/index.js
+++ packages/change-notification-sender-fetch/index.js
@@ -2,22 +2,26 @@ import fetch from 'isomorphic-fetch';
 
 export const changeNotificationSenderFetch = ({ url, jwt }) => ({
   sendChangeNotification: (correlationId, content) => {
-    const headers = { 'Content-Type': 'application/json' };
-    if (jwt) {
-      headers.Authorization = `Bearer ${jwt}`;
-    }
     content.correlationId = correlationId;
-    return fetch(url, {
-      method: 'POST',
-      headers,
-      body: JSON.stringify(content),
-    }).then((res) => {
-      if (!res.ok) {
-        throw new Error(
-          `[${correlationId}] Fetch error: ${res.status}/${res.statusText}`,
-        );
-      }
-      return res;
-    });
+    return Promise.resolve(typeof jwt === 'function' ? jwt() : jwt).then(
+      (token) => {
+        const headers = { 'Content-Type': 'application/json' };
+        if (token) {
+          headers.Authorization = `Bearer ${token}`;
+        }
+        return fetch(url, {
+          method: 'POST',
+          headers,
+          body: JSON.stringify(content),
+        }).then((res) => {
+          if (!res.ok) {
+            throw new Error(
+              `[${correlationId}] Fetch error: ${res.status}/${res.statusText}`,
+            );
+          }
+          return res;
+        });
+      },
+    );
   },
 });
